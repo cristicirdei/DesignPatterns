b6af79c1ed130e924bdbadac0f3fc80dbdfb4988
cristian.cirdei01@e-uvt.ro <83004817+cris-alt@users.noreply.github.com>
2022-10-26 13:18:11 +0300

    issues fixed by implementing Element interface

--- src/SubChapter.java
+++ src/SubChapter.java
@@ -1,35 +1,28 @@
 import java.util.ArrayList;
 
-public class SubChapter {
+public class SubChapter{
     private String name;
-    private ArrayList<Paragraph> paragraphs = new ArrayList<Paragraph>();
-    private ArrayList<Table> tables = new ArrayList<Table>();
-    private ArrayList<Image> images = new ArrayList<Image>();
-
+    private ArrayList<Element> elements = new ArrayList<Element>();
 
     public SubChapter(String name) {
         this.name = name;
     }
 
     public void createNewParagraph(String text) {
-        paragraphs.add(new Paragraph(text));
+        elements.add(new Paragraph(text));
     }
 
     public void createNewImage(String imageName) {
-        images.add(new Image(imageName));
+        elements.add(new Image(imageName));
     }
 
     public void createNewTable(String title) {
-        tables.add(new Table(title));
+        elements.add(new Table(title));
     }
 
     public void print() {
         System.out.println("Subchapter: " + this.name);
-        for(Paragraph p : paragraphs)
-            p.print();
-        for(Image i : images)
-            i.print();
-        for(Table t : tables)
-            t.print();
+        for(Element e : elements)
+            e.print();
     }
 }
